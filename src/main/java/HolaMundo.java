b4a0132ab742700093270952826bdbecdf46e79d
mjosepc <m.powell01@ufromail.cl>
2024-09-02 17:43:01 -0400

    Initial Commit

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,8 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
       System.out.println("Hola Mundo!!!!");
-      System.out.println("Francisco Lizama");
-      System.out.println("Gustavo Pérez");
-      System.out.println("Lucas Colomera");
     }
 }
