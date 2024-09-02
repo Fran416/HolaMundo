09e46c2765dc51cde18671f3e5b71ade5a525eb0
mjosepc <m.powell01@ufromail.cl>
2024-09-02 17:49:46 -0400

    Se suben nueva version java

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,6 +1,9 @@
 public class HolaMundo {
     public static void main(String[] args) {
       System.out.println("Hola Mundo!!!!");
+      System.out.println("Francisco Lizama");
+      System.out.println("Gustavo Pérez");
+      System.out.println("Lucas Colomera");
       System.out.println("Benjamin Garces");
     }
 }
